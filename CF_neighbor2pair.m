5e0377087d0219a735edbce7832491688a7a77b8
Bolei Zhou <zhoubolei@gmail.com>
2013-01-28 01:48:22 -0800

    Update CF_neighbor2pair.m

--- CF_neighbor2pair.m
+++ CF_neighbor2pair.m
@@ -3,12 +3,10 @@ function [pairSet,correSet] = CF_neighbor2pair(neighborSet,correlationSet,d)
 zeroNeighborSet=neighborSet{1,1};
 nPoint=size(zeroNeighborSet,1);
 
-
 pairSet=[];
 correSet=[];
 
 for i=1:nPoint  
-    
     curIntersect=zeroNeighborSet(i,:);
     curCorre=zeros(1,size(zeroNeighborSet,2));
     for j=1:d
@@ -16,11 +14,8 @@ for i=1:nPoint
         nextCorreSet=correlationSet{1,j};
         [curIntersect,ia,ib]=intersect(curIntersect,nextNeighborSet(i,:)); % the intersection of neighborhood
         curCorre=curCorre(1,ia)+nextCorreSet(i,ib);
-    end   
-   
-    if ~isempty(curIntersect)
-
-        
+    end     
+    if ~isempty(curIntersect)        
         correSet=[correSet curCorre./d];
         pairSet=[pairSet [i*ones(1,length(curIntersect));curIntersect]];  
     end
